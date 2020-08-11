32686f8260e99c21fd42840f82027ad23db0e9d7
wezley <wezley.singleton@gmail.com>
2020-08-11 18:08:47 -0400

    implemented login screen functionality

---
+++ src/main/java/com/revature/revabooks/models/Role.java
@@ -0,0 +1,8 @@
+package com.revature.revabooks.models;
+
+public enum Role {
+
+    // values declared within enums are constants and are comma separated
+    ADMIN, MANAGER, PREMIUM_MEMBER, BASIC_MEMBER, LOCKED;
+
+}
