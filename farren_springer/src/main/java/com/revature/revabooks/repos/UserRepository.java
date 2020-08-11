dd69106d2ee2d0b8215690f6a4bce89249a0d95d
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-08-11 17:09:04 -0500

    Implemented login screen functionality

---
+++ farren_springer/src/main/java/com/revature/revabooks/repos/UserRepository.java
@@ -0,0 +1,18 @@
+package com.revature.revabooks.repos;
+
+import com.revature.revabooks.models.AppUser;
+import com.revature.revabooks.models.Role;
+
+public class UserRepository {
+
+    // TODO replace mock implementation with in-memory persistence
+    public AppUser findUserByCredentials(String username, String password) {
+
+        if (!username.equals("admin") && !password.equals("p4ssw0rd")) {
+            return null;
+        }
+
+        return new AppUser(1, "Adam", "Inn", "admin", "p4ssw0rd", Role.ADMIN);
+    }
+
+}
---
+++ farren_springer/src/main/java/com/revature/revabooks/services/UserService.java
@@ -0,0 +1,32 @@
+package com.revature.revabooks.services;
+
+import com.revature.revabooks.models.AppUser;
+import com.revature.revabooks.repos.UserRepository;
+
+public class UserService {
+
+    private UserRepository userRepo;
+
+    public UserService() {
+        System.out.println("[LOG] - Instantiating " + this.getClass().getName());
+        userRepo = new UserRepository();
+    }
+
+    public AppUser authenticate(String username, String password) {
+        // handle edge cases first!
+
+        if (username == null || username.trim().equals("") || password == null || password.trim().equals("")) {
+            // TODO implement a custom InvalidRequestException
+            throw new RuntimeException("Invalid credential values provided!");
+        }
+
+        AppUser authenticatedUser = userRepo.findUserByCredentials(username, password);
+
+        if (authenticatedUser == null) {
+            // TODO implement a custom AuthenticationException
+            throw new RuntimeException("No user found with the provided credentials");
+        }
+
+        return authenticatedUser;
+    }
+}
