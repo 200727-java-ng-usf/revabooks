ff4c04a6f9b22abd7d136c2e3ee8b4dc162e9606
wezley <wezley.singleton@gmail.com>
2020-08-12 17:59:03 -0400

    added some basic unit tests for userService.authenticate

--- src/test/java/com/revature/revabooks/services/UserServiceTests.java
+++ src/test/java/com/revature/revabooks/services/UserServiceTests.java
@@ -1,8 +1,14 @@
 package com.revature.revabooks.services;
 
+import com.revature.revabooks.exceptions.AuthenticationException;
+import com.revature.revabooks.exceptions.InvalidRequestException;
 import com.revature.revabooks.models.AppUser;
+import com.revature.revabooks.models.Role;
 import com.revature.revabooks.repos.UserRepository;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.util.HashSet;
@@ -16,7 +22,51 @@ public class UserServiceTests {
 
     @Before
     public void setup() {
-//        sut = new UserService();
+        sut = new UserService(mockUserRepo);
+        mockUsers.add(new AppUser(1, "Adam", "Inn", "admin", "secret", Role.ADMIN));
+        mockUsers.add(new AppUser(2, "Manny", "Gerr", "manager", "manage", Role.MANAGER));
+        mockUsers.add(new AppUser(3, "Alice", "Anderson", "aanderson", "password", Role.BASIC_MEMBER));
+        mockUsers.add(new AppUser(4, "Bob", "Bailey", "bbailey", "dev", Role.PREMIUM_MEMBER));
+    }
+
+    @After
+    public void tearDown() {
+        sut = null;
+        mockUsers.removeAll(mockUsers);
+    }
+
+    @Test
+    public void authenticationWithValidCredentials() {
+
+        // Arrange
+        AppUser expectedUser = new AppUser(1, "Adam", "Inn", "admin", "secret", Role.ADMIN);
+        Mockito.when(mockUserRepo.findUserByCredentials("admin", "secret"))
+                .thenReturn(expectedUser);
+
+        // Act
+        AppUser actualResult = sut.authenticate("admin", "secret");
+
+        // Assert
+        Assert.assertEquals(expectedUser, actualResult);
+
+    }
+
+    @Test(expected = InvalidRequestException.class)
+    public void authenticationWithInvalidCredentials() {
+
+        // Arrange
+        // nothing to do here for this test; nothing to mock or expect
+
+        // Act
+        sut.authenticate("", "");
+
+        // Assert
+        // nothing here, because the method should have raised an exception
+
+    }
+
+    @Test(expected = AuthenticationException.class)
+    public void authenticationWithUnknownCredentials() {
 
     }
 }
