dea2b39c589946c7c073832ed8b2566597311cd3
Shea Wilson <sheawilsn@gmail.com>
2020-08-12 16:57:28 -0500

    needs work

---
+++ revabooks/src/main/java/com/revature/revabooks/exceptions/AuthenicationException.java
@@ -0,0 +1,9 @@
+package com.revature.revabooks.exceptions;
+
+public class AuthenicationException extends RuntimeException {
+
+//    public AuthenticationException(String message) {
+//        super(message);
+    }
+
+}
--- revabooks/src/test/java/com/revature/revabooks/UserServiceTest.java
+++ revabooks/src/test/java/com/revature/revabooks/UserServiceTest.java
@@ -2,8 +2,12 @@ package com.revature.revabooks;
 
 
 import com.revature.revabooks.models.AppUser;
+import com.revature.revabooks.models.Role;
 import com.revature.revabooks.repos.UserRepo;
 import com.revature.revabooks.services.UserService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.util.HashSet;
@@ -13,4 +17,34 @@ public class UserServiceTest {
     private UserService sut;
     private UserRepo mockUserRepo = Mockito.mock(UserRepo.class);
     Set<AppUser> mockUsers = new HashSet<>();
+
+    @Before
+    public void setup(){
+        sut = new UserService(mockUserRepo);
+        mockUsers.add(new AppUser(1,"Adam","Inn","admin","p4ssw0rd", Role.ADMIN));
+
+    }
+
+    @After
+    public  void tearDown(){
+        sut = null;
+        mockUsers.removeAll(mockUsers);
+    }
+
+    @Test
+    public void authenticationWithValidCredentials(){
+        //Arrange
+        AppUser expectedUser = new AppUser(1,"Adam","Inn","admin","p4ssw0rd", Role.ADMIN);
+//        Mockito.when(mockUserRepo).findUserCredentials(Mockito.anyString()).then;
+        //Act
+
+        //Assert
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void authenticationWithInvalidCredentials(){
+        sut.authentic("","");
+        //Arrange (null)
+        //
+    }
 }
