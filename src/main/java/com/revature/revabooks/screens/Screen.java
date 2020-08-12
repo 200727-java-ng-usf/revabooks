75974c32dee334839f32a48242cb8167a4c23358
Chris Yanoso <christopheryanoso@gmail.com>
2020-08-12 14:42:03 -0400

    added register screen

---
+++ src/main/java/com/revature/revabooks/screens/Screen.java
@@ -0,0 +1,12 @@
+package com.revature.revabooks.screens;
+
+public abstract class Screen {
+
+    /**
+     * Displays a particular menu depending on the Screens implementation.
+     */
+
+    public abstract void render();
+
+
+}
