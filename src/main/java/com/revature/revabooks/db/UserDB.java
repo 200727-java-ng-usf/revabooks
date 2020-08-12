e083d67b5a0d4eea37d5cb0b5182a6131c7e9b1e
EPaetow <elipaetow@yahoo.com>
2020-08-12 12:07:52 -0500

    user repo update

---
+++ src/main/java/com/revature/revabooks/db/UserDB.java
@@ -0,0 +1,42 @@
+package com.revature.revabooks.db;
+
+import com.revature.revabooks.models.AppUser;
+import com.revature.revabooks.models.Role;
+
+import java.util.HashMap;
+
+public class UserDB extends HashMap<Integer, AppUser> {
+
+    public static UserDB userDataset = new UserDB();
+    public static int key =1;
+
+    static {
+        userDataset.addUser(new AppUser("adam", "Inn", "admin" ,"p4ssw0rd", Role.ADMIN));
+        userDataset.addUser(new AppUser("eli", "p", "ep" ,"123", Role.MANAGER));
+        userDataset.addUser(new AppUser("cpt", "falcon", "punch" ,"blueFalcon", Role.BASIC_MEMBER));
+        userDataset.addUser(new AppUser("luigi", "mario", "bros" ,"fireball", Role.PREMIUM_MEMBER));
+        userDataset.addUser(new AppUser("darth", "vader", "father" ,"darkside", Role.BASIC_MEMBER));
+
+
+
+
+    }
+    public AppUser addUser(AppUser newUser){
+        AppUser nUser = new AppUser(newUser);
+        nUser.setId(key);
+        userDataset.put(key++, nUser);
+        return nUser;
+    }
+
+    public AppUser findUserByCredential(String username, String password){
+
+
+
+        for (AppUser user: userDataset.values()){
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)){
+                return user;
+            }
+        }
+        return null;
+    }
+}
--- src/main/java/com/revature/revabooks/repos/UserRepository.java
+++ src/main/java/com/revature/revabooks/repos/UserRepository.java
@@ -1,14 +1,13 @@
 package com.revature.revabooks.repos;
 
+import com.revature.revabooks.db.UserDB;
 import com.revature.revabooks.models.AppUser;
 import com.revature.revabooks.models.Role;
 
 public class UserRepository {
+    private UserDB userDataset = UserDB.userDataset;
 
     public AppUser findUserByCredentials(String username, String password){
-        if (!username.equals("admin") && !password.equals("p4ssw0rd")){
-            return null;
-        }
-        return new AppUser(1, "adam", "inn", "admin", "p4ssw0rd", Role.ADMIN);
+        return userDataset.findUserByCredential(username, password);
     }
 }
