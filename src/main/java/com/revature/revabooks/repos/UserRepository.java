ba395c88b78cdab925c72a8a66e6b079a33d73d3
wezley <wezley.singleton@gmail.com>
2020-08-21 11:11:08 -0400

    added mapresultSet method, and impl getUserByUsername

--- src/main/java/com/revature/revabooks/repos/UserRepository.java
+++ src/main/java/com/revature/revabooks/repos/UserRepository.java
@@ -1,14 +1,16 @@
 package com.revature.revabooks.repos;
 
 import com.revature.revabooks.models.AppUser;
+import com.revature.revabooks.models.Role;
 import com.revature.revabooks.util.ConnectionFactory;
 
-import javax.swing.text.html.Option;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 public class UserRepository {
 
@@ -20,42 +22,71 @@ public class UserRepository {
 
         Optional<AppUser> _user = Optional.empty();
 
-
         try (Connection conn = ConnectionFactory.getInstance().getConnection()) {
 
-            String sql = "SELECT * FROM revabooks.app_users WHERE username = ? AND password = ?";
+            String sql = "SELECT * FROM revabooks.app_users au " +
+                         "JOIN revabooks.user_roles ur " +
+                         "ON au.role_id = ur.id " +
+                         "WHERE username = ? AND password = ?";
+
             PreparedStatement pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, username);
             pstmt.setString(2, password);
 
             ResultSet rs = pstmt.executeQuery();
 
-            AppUser appUser = new AppUser();
-
-            while(rs.next()) {
-                appUser.setId(rs.getInt("id"));
-                appUser.setUsername(rs.getString("username"));
-                appUser.setPassword(rs.getString("password"));
-                appUser.setFirstName(rs.getString("first_name"));
-                appUser.setLastName(rs.getString("last_name"));
-            }
-
-            _user = Optional.of(appUser);
+            _user = mapResultSet(rs).stream().findFirst();
 
         } catch (SQLException sqle) {
             sqle.printStackTrace();
         }
 
-
         return _user;
     }
 
     public Optional<AppUser> findUserByUsername(String username) {
-        return Optional.of(null);
+
+        Optional<AppUser> _user = Optional.empty();
+
+        try (Connection conn = ConnectionFactory.getInstance().getConnection()) {
+
+            String sql = "SELECT * FROM revabooks.app_users WHERE username = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, username);
+
+            ResultSet rs = pstmt.executeQuery();
+            _user = mapResultSet(rs).stream().findFirst();
+
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+
+        return _user;
+
     }
 
     public Optional<AppUser> save(AppUser newUser) {
         return Optional.of(null);
+    }
+
+    private Set<AppUser> mapResultSet(ResultSet rs) throws SQLException {
+
+        Set<AppUser> users = new HashSet<>();
+
+        while (rs.next()) {
+            AppUser temp = new AppUser();
+            temp.setId(rs.getInt("id"));
+            temp.setFirstName(rs.getString("first_name"));
+            temp.setLastName(rs.getString("last_name"));
+            temp.setUsername(rs.getString("username"));
+            temp.setPassword(rs.getString("password"));
+            temp.setRole(Role.getByName(rs.getString("name")));
+            System.out.println(temp);
+            users.add(temp);
+        }
+
+        return users;
+
     }
 
 }
