72906ff9e5654fbe40b85a464e48e4d19f45d3e9
wezley <wezley.singleton@gmail.com>
2020-09-10 14:03:46 -0400

    added validation to login and reg forms

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -38,6 +38,7 @@ function loadRegister() {
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
             APP_VIEW.innerHTML = xhr.responseText;
+            configureRegisterView();
         }
     }
 
@@ -50,7 +51,23 @@ function configureLoginView() {
     console.log('in configureLoginView()');
 
     document.getElementById('login-message').setAttribute('hidden', true);
+    document.getElementById('login-button-container').addEventListener('mouseover', validateLoginForm);
     document.getElementById('login').addEventListener('click', login);
+
+}
+
+function configureRegisterView() {
+
+    console.log('in configureRegisterView()');
+
+    document.getElementById('reg-message').setAttribute('hidden', true);
+
+    document.getElementById('reg-username').addEventListener('blur', isUsernameAvailable);
+    document.getElementById('email').addEventListener('blur', isEmailAvailable);
+
+    document.getElementById('register').setAttribute('disabled', true);
+    document.getElementById('reg-button-container').addEventListener('mouseover', validateRegisterForm);
+    document.getElementById('register').addEventListener('click', register);
 
 }
 
@@ -78,8 +95,6 @@ function login() {
 
     xhr.onreadystatechange = function () {
 
-        console.log(xhr.readyState);
-
         if (xhr.readyState == 4 && xhr.status == 204) {
 
             document.getElementById('login-message').setAttribute('hidden', true);
@@ -95,4 +110,152 @@ function login() {
 
     }
 
+}
+
+function register() {
+
+    console.log('in register()');
+
+    let fn = document.getElementById('fn').value;
+    let ln = document.getElementById('ln').value;
+    let email = document.getElementById('email').value;
+    let un = document.getElementById('reg-username').value;
+    let pw = document.getElementById('reg-password').value;
+
+    let newUser = {
+        firstName: fn,
+        lastName: ln,
+        email: email,
+        username: un,
+        password: pw
+    }
+
+    let newUserJSON = JSON.stringify(newUser);
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('POST', 'users');
+    xhr.send(newUserJSON);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 201) {
+            let registeredUser = JSON.parse(xhr.responseText);
+            console.log(registeredUser);
+        } else if (xhr.readyState == 4 && xhr.status != 201) {
+            document.getElementById('reg-message').removeAttribute('hidden');
+            let err = JSON.parse(xhr.responseText);
+            document.getElementById('reg-message').innerText = err.message;
+        }
+    }
+    
+
+}
+
+function logout() {
+    
+    console.log('in logout()');
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'auth');
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 204) {
+            console.log('logout successful!');
+        }
+    }
+}
+
+function isUsernameAvailable() {
+
+    console.log('in isUsernameAvailable()');
+
+    let username = document.getElementById('reg-username').value;
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('POST', 'username.validate');
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.send(JSON.stringify(username));
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 204) {
+            console.log('Provided username is available!');
+        } else if (xhr.readyState == 4 && xhr.status == 409 ) {
+            document.getElementById('reg-message').removeAttribute('hidden')
+            document.getElementById('reg-message').innerText = 'The provided username is already taken!';
+            document.getElementById('register').setAttribute('disabled', true);
+        }
+    }
+
+}
+
+function isEmailAvailable() {
+
+    console.log('in isEmailAvailable()');
+
+    let email = document.getElementById('email').value;
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('POST', 'email.validate');
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.send(JSON.stringify(email));
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 204) {
+            console.log('Provided email is available!');
+        } else if (xhr.readyState == 4 && xhr.status == 409) {
+            document.getElementById('reg-message').removeAttribute('hidden');
+            document.getElementById('reg-message').innerText = 'The provided email address is already taken!';
+            document.getElementById('register').setAttribute('disabled', true);
+        }
+    }
+}
+
+//---------------------FORM VALIDATION-------------------------
+
+function validateLoginForm() {
+
+    console.log('in validateLoginForm()');
+
+    let msg = document.getElementById('login-message').innerText;
+
+    if (msg == 'User authentication failed!') {
+        return;
+    }
+
+    let un = document.getElementById('login-username').value;
+    let pw = document.getElementById('login-password').value;
+
+    if (!un || !pw) {
+        document.getElementById('login-message').removeAttribute('hidden');
+        document.getElementById('login-message').innerText = 'You must provided values for all fields in the form!'
+        document.getElementById('login').setAttribute('disabled', true);
+    } else {
+        document.getElementById('login').removeAttribute('disabled');
+        document.getElementById('login-message').setAttribute('hidden', true);
+    }
+
+}
+
+function validateRegisterForm() {
+
+    console.log('in validateRegisterForm()');
+
+    let fn = document.getElementById('fn').value;
+    let ln = document.getElementById('ln').value;
+    let email = document.getElementById('email').value;
+    let un = document.getElementById('reg-username').value;
+    let pw = document.getElementById('reg-password').value;
+
+    if (!fn || !ln || !email || !un || !pw) {
+        document.getElementById('reg-message').removeAttribute('hidden');
+        document.getElementById('reg-message').innerText = 'You must provided values for all fields in the form!'
+        document.getElementById('register').setAttribute('disabled', true);
+    } else {
+        document.getElementById('register').removeAttribute('disabled');
+        document.getElementById('reg-message').setAttribute('hidden', true);
+    }
 }
