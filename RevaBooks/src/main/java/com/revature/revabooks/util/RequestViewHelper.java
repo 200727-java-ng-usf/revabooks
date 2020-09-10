54abc7e6e87ac926fac00e773f5d2b008e292c82
Stephen Brasel <wearyroad@gmail.com>
2020-09-10 17:42:48 -0400

    modified endpoints

--- RevaBooks/src/main/java/com/revature/revabooks/util/RequestViewHelper.java
+++ RevaBooks/src/main/java/com/revature/revabooks/util/RequestViewHelper.java
@@ -8,12 +8,15 @@ public class RequestViewHelper {
 	public String process(HttpServletRequest req){
 		switch (req.getRequestURI()){
 			case "/revabooks/login.view":
+			case "/login.view":
 				return "partials/login.html";
 
 			case "/revabooks/register.view":
+			case "/register.view":
 				return "partials/register.html";
 
 			case "/revabooks/home.view":
+			case "/home.view":
 				String principal = (String) req.getSession().getAttribute("principal");
 				if(principal == null || principal.equals("")){
 					return "partials/login.html";
