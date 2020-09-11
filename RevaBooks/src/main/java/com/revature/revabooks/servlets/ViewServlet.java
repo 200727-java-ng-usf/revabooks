1fc0a151372aa4b58980159191313e1151d88caa
Stephen Brasel <wearyroad@gmail.com>
2020-09-11 11:29:36 -0400

    edited doGet in ViewServlet

--- RevaBooks/src/main/java/com/revature/revabooks/servlets/ViewServlet.java
+++ RevaBooks/src/main/java/com/revature/revabooks/servlets/ViewServlet.java
@@ -2,6 +2,7 @@ package com.revature.revabooks.servlets;
 
 import com.revature.revabooks.util.RequestViewHelper;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,7 +16,8 @@ public class ViewServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String nextView = new RequestViewHelper().process(req);
-		req.getRequestDispatcher(nextView).forward(req, resp);
+		RequestDispatcher requestDispatcher = req.getRequestDispatcher(nextView);
+		requestDispatcher.forward(req, resp);
 	}
 
 }
